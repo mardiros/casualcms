6500d287d76432ad6a6d9a872f9e704b07ab4366
Guillaume Gauvrit <guillaume@gauvr.it>
2022-04-02 16:03:03 +0200

    Add the AppContext that wrap a config

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { AppContext, AppConfig } from "./config";
+import { initDB } from "./casualcms/adapters/repositories"
+import { Api } from "./casualcms/service/api";
+import { UnitOfWork } from "./casualcms/service/uow";
 
 import "./css/index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const init = async (): Promise<AppConfig> => {
+  const db = await initDB()
+  const config = new AppConfig(
+    new UnitOfWork(db),
+    new Api(),
+  );
+  return config
+}
+
+init().then(
+  async (config: AppConfig) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <AppContext.Provider value={config}>
+          <App />
+        </AppContext.Provider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
--- tests/frontend/app.test.tsx
+++ tests/frontend/app.test.tsx
@@ -2,10 +2,16 @@ import { expect } from "chai";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "../../src/frontend/App";
+import { AppContext } from "../../src/frontend/config";
+import config from "./config"
 
 describe("As a user, I am redirect to the login page", () => {
   it("cast a single error", () => {
-    render(<App />);
+    render(
+      <AppContext.Provider value={config}>
+        <App />
+      </AppContext.Provider>
+    );
     let input = screen.getByLabelText("Username:");
     expect(input).not.equal(null);
   })

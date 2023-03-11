86c7a0255f59db8696c2c678a6ce66465165c912
Guillaume Gauvrit <guillaume@gauvr.it>
2023-03-11 17:39:58 +0100

    Add flat icon as a logo instead of emoji

--- src/webapp/ui/components/header.tsx
+++ src/webapp/ui/components/header.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { Box, Flex, Heading, Spacer } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../pages/login/hooks";
 import { AccountMenu } from "./account_menu";
+import { TShirt64 } from "../images/tshirt64";
 
 export const Header: React.FunctionComponent<{}> = (): React.ReactElement => {
   let auth = useAuth();
@@ -9,7 +11,10 @@ export const Header: React.FunctionComponent<{}> = (): React.ReactElement => {
     <Flex minWidth="max-content" alignItems="center" gap="2">
       <Box p="2">
         <Heading size="lg" color="cyan.50">
-          <Link to="/admin">👕 Casual CMS</Link>
+          <Link to="/admin" style={{ "display": "flex", "alignItems": "flex-end", "columnGap": "1rem" }}>
+            <TShirt64 />
+            Casual CMS
+          </Link>
         </Heading>
       </Box>
       <Spacer />
---
+++ src/webapp/ui/images/tshirt64.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Icon from "url:../../svg/t-shirt-64.svg";
+
+
+export const TShirt64: React.FunctionComponent<{}> = (): React.ReactElement => {
+  return (
+    <img src={Icon} alt="casual cms icon" />
+  )
+}
